98d8cf27868f0e5478892695b9f51c6060be4fb0
fabianrm21 <fabianrm@umich.edu>
2024-11-21 16:10:01 -0500

    office hours join

--- Join.cpp
+++ Join.cpp
@@ -1,4 +1,5 @@
 #include "Join.hpp"
+#include "constants.hpp"
 #include <vector>
 
 using namespace std;
@@ -17,8 +18,8 @@ vector<Bucket> partition(Disk* disk, Mem* mem, pair<uint, uint> left_rel,
 
     // Process the left relation
     for (uint page_id = left_rel.first; page_id < left_rel.second; ++page_id) {
-        mem->loadFromDisk(disk, page_id, 0); // Load disk page to memory page 0
-        Page* page = mem->mem_page(0);
+        mem->loadFromDisk(disk, page_id, MEM_SIZE_IN_PAGE - 1); // Load disk page to memory page 0
+        Page* page = mem->mem_page(MEM_SIZE_IN_PAGE - 1);
         for (uint i = 0; i < page->size(); ++i) {
             Record record = page->get_record(i);
             uint bucket_id = record.partition_hash() % (MEM_SIZE_IN_PAGE - 1);
@@ -34,8 +35,8 @@ vector<Bucket> partition(Disk* disk, Mem* mem, pair<uint, uint> left_rel,
 				output_page->reset();
 			}
     	}
-		mem->reset();
     }
+    mem->reset();
 	// Flush remaining pages for left relation
     for (uint bucket_id = 0; bucket_id < partitions.size(); ++bucket_id) {
         Page* output_page = mem->mem_page(bucket_id + 1);
@@ -46,11 +47,10 @@ vector<Bucket> partition(Disk* disk, Mem* mem, pair<uint, uint> left_rel,
         }
     }
 
-
     // Process the right relation
     for (uint page_id = right_rel.first; page_id < right_rel.second; ++page_id) {
-        mem->loadFromDisk(disk, page_id, 0); // Load disk page to memory page 0
-        Page* page = mem->mem_page(0);
+        mem->loadFromDisk(disk, page_id, MEM_SIZE_IN_PAGE - 1); // Load disk page to memory page 0
+        Page* page = mem->mem_page(MEM_SIZE_IN_PAGE - 1);
         for (uint i = 0; i < page->size(); ++i) {
             Record record = page->get_record(i);
             uint bucket_id = record.partition_hash() % (MEM_SIZE_IN_PAGE - 1);
@@ -66,8 +66,8 @@ vector<Bucket> partition(Disk* disk, Mem* mem, pair<uint, uint> left_rel,
 				output_page->reset();
 			}
 		}
-		mem->reset();
     }
+    mem->reset();
 	// Flush remaining pages for right relation
     for (uint bucket_id = 0; bucket_id < partitions.size(); ++bucket_id) {
         Page* output_page = mem->mem_page(bucket_id + 1);
@@ -89,35 +89,47 @@ vector<uint> probe(Disk* disk, Mem* mem, vector<Bucket>& partitions) {
     vector<uint> disk_pages;
 
     for (Bucket& bucket : partitions) {
-        vector<uint> left_pages = bucket.get_left_rel();
-        vector<uint> right_pages = bucket.get_right_rel();
+        // determine which is the bigger relation
+        // vector<uint> left_pages = bucket.get_left_rel();
+        // vector<uint> right_pages = bucket.get_right_rel();
+
+        vector<uint> big_rel;
+        vector<uint> small_rel;
+
+        if (bucket.num_left_rel_record > bucket.num_right_rel_record) {
+            big_rel = bucket.get_left_rel();
+            small_rel = bucket.get_right_rel();
+        } else {
+            big_rel = bucket.get_right_rel();
+            small_rel = bucket.get_left_rel();
+        }
 
         // Clear memory for each bucket processing
-        mem->reset();
+        // mem->reset();
 
         // Load left relation records into memory
-        for (uint left_page_id : left_pages) {
-            mem->loadFromDisk(disk, left_page_id, 0);
+        for (uint left_page_id : small_rel) {
+            mem->loadFromDisk(disk, left_page_id, MEM_SIZE_IN_PAGE - 1);
         }
 
         // Perform join with right relation
-        for (uint right_page_id : right_pages) {
-            mem->loadFromDisk(disk, right_page_id, 1);
-            Page* right_page = mem->mem_page(1);
+        for (uint right_page_id : big_rel) {
+            mem->loadFromDisk(disk, right_page_id, MEM_SIZE_IN_PAGE - 1);
+            Page* right_page = mem->mem_page(MEM_SIZE_IN_PAGE - 1);
 
             for (uint i = 0; i < right_page->size(); ++i) {
                 Record right_record = right_page->get_record(i);
-                uint right_hash = right_record.probe_hash();
+                uint right_hash = right_record.probe_hash() % (MEM_SIZE_IN_PAGE - 2);
 
-                for (uint j = 0; j < mem->mem_page(0)->size(); ++j) {
-                    Record left_record = mem->mem_page(0)->get_record(j);
-                    uint left_hash = left_record.probe_hash();
+                for (uint j = 0; j < mem->mem_page(MEM_SIZE_IN_PAGE - 1)->size(); ++j) {
+                    Record left_record = mem->mem_page(MEM_SIZE_IN_PAGE - 1)->get_record(j);
+                    uint left_hash = left_record.probe_hash() % (MEM_SIZE_IN_PAGE - 2);
 
                     // Check if hashes match and keys match
                     if (left_hash == right_hash && left_record == right_record) {
-                        Page* output_page = mem->mem_page(2);
+                        Page* output_page = mem->mem_page(MEM_SIZE_IN_PAGE - 1);
                         if (output_page->full()) {
-                            uint page_id = mem->flushToDisk(disk, 2);
+                            uint page_id = mem->flushToDisk(disk, MEM_SIZE_IN_PAGE - 1);
                             disk_pages.push_back(page_id);
                         }
                         output_page->loadPair(left_record, right_record);
@@ -126,16 +138,17 @@ vector<uint> probe(Disk* disk, Mem* mem, vector<Bucket>& partitions) {
             }
         }
 		// Flush remaining output page
-		if (mem->mem_page(2)->full()) {
-			uint page_id = mem->flushToDisk(disk, 2);
-			disk_pages.push_back(page_id);
-		}
+		// if (mem->mem_page(MEM_SIZE_IN_PAGE - 1)->full()) {
+		// 	uint page_id = mem->flushToDisk(disk, MEM_SIZE_IN_PAGE - 1);
+		// 	disk_pages.push_back(page_id);
+		// }
+        mem->reset();
     }
 	// Flush remaining output page
 	// flush todo lo que quede, no solo este
-	if (!mem->mem_page(2)->empty()) {
-		uint page_id = mem->flushToDisk(disk, 2);
-		disk_pages.push_back(page_id);
-	}
+	// if (!mem->mem_page(MEM_SIZE_IN_PAGE - 1)->empty()) {
+	// 	uint page_id = mem->flushToDisk(disk, MEM_SIZE_IN_PAGE - 1);
+	// 	disk_pages.push_back(page_id);
+	// }
     return disk_pages;
 }
